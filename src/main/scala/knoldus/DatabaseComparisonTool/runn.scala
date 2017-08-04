633dba64fc7845ac9f6eac8c5c082d70323cdcc2
Akshansh Jain <akshansh.jain@knoldus.in>
2017-08-04 10:23:25 +0530

    Added path to input files

--- src/main/scala/knoldus/DatabaseComparisonTool/runn.scala
+++ src/main/scala/knoldus/DatabaseComparisonTool/runn.scala
@@ -5,9 +5,9 @@ package knoldus.DatabaseComparisonTool
 object MainClass extends App with Database {
 
 
-  val mySqlTuple = MySQLService.queryTime("/home/knoldus/CSVInputFiles/MySQLDDL.csv")
-  val postgreSQLTuple = PostgreSQLService.queryTime("/home/knoldus/CSVInputFiles/PostgreSQLDDL.csv")
-  val SQLiteTuple = SQLiteService.queryTime("/home/knoldus/CSVInputFiles/SQLiteDDL.csv")
+  val mySqlTuple = MySQLService.queryTime("/src/test/resources/CSVInputFiles/MySQLDDL.csv")
+  val postgreSQLTuple = PostgreSQLService.queryTime("/src/test/resources/CSVInputFiles/PostgreSQLDDL.csv")
+  val SQLiteTuple = SQLiteService.queryTime("/src/test/resources/CSVInputFiles/SQLiteDDL.csv")
 
   WriteFinalFileService.writeFinalFile(mySqlTuple._1, mySqlTuple._2, postgreSQLTuple._2, SQLiteTuple._2)
   
